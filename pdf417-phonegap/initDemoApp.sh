4a0ea2e71fa3eccc5fe9a9cd273a8c12e8c3d445
Julio César <jcesarmobile@gmail.com>
2018-04-18 20:09:26 +0200

    Add pdf folder

---
+++ pdf417-phonegap/initDemoApp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+APP_NAME="PDF417Demo"
+
+# position to a relative path
+HERE="$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")"
+pushd $HERE > /dev/null
+
+# remove any existing code
+rm -rf $APP_NAME
+
+# create a sample application
+cordova create $APP_NAME mobi.pdf417.demo $APP_NAME
+
+# enter into demo project folder
+cd $APP_NAME
+
+# add the PDF417 plugin
+cordova plugin add ../Pdf417 --variable CAMERA_USAGE_DESCRIPTION="Camera permission is required for automated scanning"
+
+# add ios, android support to the project
+cordova platform add android@7
+cordova platform add ios
+
+# copy www folder
+cp  -f -r ../www .
+
+# build app
+cordova prepare
+
+# how to run
+echo "To run iOS demo application open Xcode project $APP_NAME.xcodeproj"
+echo "To run Android demo application, position to $APP_NAME folder and type: cordova run android"
